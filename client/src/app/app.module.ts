df85a7fc51df0b67ecaf62a3abd11e8b82bd429d
Leonardo Bevilacqua <leonardo.bevilacqua@eldorado.org.br>
2021-12-20 14:52:28 -0300

    Adding header and create category and device page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { DeviceComponent } from './pages/device/device.component';
+
+const appRoutes: Routes = [
+  { path: '', component: DeviceComponent },
+  { path: 'category', component: CategoryComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoryComponent,
+    DeviceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
