80431e0e0297737d60908f127ee08731327ce44f
Ubuntu <ubuntu@ip-172-31-13-150.sa-east-1.compute.internal>
2021-12-21 16:49:23 +0000

    checking cors issue

--- server/src/server.js
+++ server/src/server.js
@@ -7,10 +7,11 @@ import deviceController from './controllers/device.controller.js';
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:4200',
+  origin: ['http://localhost:4200', 'https://aws-challenge-front.s3.sa-east-1.amazonaws.com']
 };
 
 const PORT = process.env.PORT || 5000;
+const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 
 app.use(cors(corsOptions));
 // parse request of content-type - application/json
@@ -25,7 +26,7 @@ app.use('/test', testController);
 app.use('/category', categoryController);
 app.use('/device', deviceController);
 
-app.listen(PORT, () => {
+app.listen(PORT, HOSTNAME, () => {
   // eslint-disable-next-line no-console
   console.log(`Server is running on port ${PORT}`);
 });
